20d03b8784eff056c4ea21fbf2a0cf3dfd36aaaa
Hasibur14 <hasib141415@gmail.com>
2025-01-20 14:05:18 +0600

    [ADD] conncet db

--- index.js
+++ index.js
@@ -2,41 +2,29 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const multer = require("multer");
-const upload = multer({ dest: "public/assets/" });
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const { restart } = require("nodemon");
 
-// MongoDB connection
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.766g6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-let db, usersCollection;
-
-async function connectDB() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-    db = client.db("TestDB"); // Replace <dbname> with your database name
-    usersCollection = db.collection("users"); // Replace 'users' with your collection name
-  } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
-  }
-}
-connectDB();
 async function run() {
   try {
-
 
     // DATA COLLECTION
 
@@ -90,17 +78,13 @@ async function run() {
 
 
 
-
-
-
-    //await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+
   }
 }
 run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
   res.send("ASBRL is Running");
